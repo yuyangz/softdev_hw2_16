41fc0f8f253ad745e4742ab60af7cdbe45af14a3
Yuyang Zhang <yzhang4@stuy.edu>
2018-04-26 13:16:00 -0400

    Added intersection and union

--- tasks.py
+++ tasks.py
@@ -1,1 +1,16 @@
-yuh	
+#Jawad Kadir & Yuyang Zhang
+#SoftDev2 pd07
+#K16 -- Ready, Set, math!
+#2018-04-26
+
+#take x in a and take x from b that is not in a
+def union(a,b):
+    return [x for x in a +[ x for x in b if x not in a]]
+
+print union([1,2,3],[3,5,6])
+
+#take x that is both in a and b
+def intersection(a,b):
+    return [x for x in a if x in b]
+
+print intersection([1,2,3],[2,3,4])
