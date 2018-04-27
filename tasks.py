e8d5c84ba0d3f3a6714b97d0252470d4a76301d9
Yuyang Zhang <yzhang4@stuy.edu>
2018-04-26 21:38:18 -0400

    The magic of kadirJ

--- tasks.py
+++ tasks.py
@@ -15,12 +15,20 @@ def intersection(a,b):
 
 print intersection([1,2,3],[2,3,4])
 
+#takes x in a that is not in b
 def set_difference(a,b):
     return [x for x in a if x not in b]
 
 print set_difference([1,2,3],[2,3,4])
 
+#takes x in a that is not in b and x in b that is not in a
 def sym_difference(a,b):
     return set_difference(a,b)+ set_difference(b,a)
 
 print sym_difference([1,2,3],[2,3,4])
+
+#pairs up each x in a and each x in b
+def cart_prod(a,b):
+    return [(x,y) for x in a for y in b]
+
+print cart_prod([1,2,3],[2,3,4])
